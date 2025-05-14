fbc987e3f52f9a48ed68cc668bdb23c46129858b
Mubashir <m.mubashirweb@gmail.com>
2025-05-14 17:09:07 +0500

    update some changes

--- src/components/CheckOutPage.jsx
+++ src/components/CheckOutPage.jsx
@@ -4,16 +4,18 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { usePostMutation } from "../services/reactQuery";
 import { toast } from "react-toastify";
 import { Button, Title } from "@mantine/core";
+import { useState } from "react";
 
 const CheckoutPage = () => {
+  const { mutate: createBookings, isPending: isCreatingBookins } =
+    usePostMutation("bookings");
+  const [isCreatingBooking, setIsCreatingBooking] = useState(false);
   const navigate = useNavigate();
   const { mutate: createPayment, isPending } = usePostMutation("payment");
   const data = JSON.parse(localStorage.getItem("data"));
   const token = localStorage.getItem("token") || {};
   const location = useLocation();
   const bookingData = location.state;
-  const { booking, merchantId } = bookingData || {};
-  //consoe.log("Booking Data", bookingData);
   const {
     name,
     email,
@@ -24,35 +26,81 @@ const CheckoutPage = () => {
     totalDuration,
     paymentMethod,
     totalPrice,
-  } = booking || {};
+  } = bookingData || {};
+
   function handlePayment() {
-    createPayment(
+    // Create the booking first
+    createBookings(
       {
-        endpoint: `/api/customer-payment`,
-        payload: {
-          price: totalPrice,
-          merchantAccountId: merchantId,
-        },
+        endpoint: "/api/create-booking",
+        payload: { ...bookingData, paymentMethod: "online" },
       },
       {
-        onSuccess: (navigate) => {
-          window.location.href = navigate.url;
-        },
-        onError: () =>
-          toast.error("Error While Creating Payment", {
+        onSuccess: (data) => {
+          toast.success("Booking Created Successfully", {
             position: "top-center",
-          }),
+          });
+
+          // Extract merchantId from the response
+
+          // Once the booking is created, trigger the payment
+          createPayment(
+            {
+              endpoint: `/api/customer-payment`,
+              payload: {
+                price: totalPrice,
+                merchantAccountId: data.merchantId, // Use the merchantId here
+              },
+            },
+            {
+              onSuccess: (navigate) => {
+                window.location.href = navigate.url;
+              },
+              onError: () =>
+                toast.error("Error While Creating Payment", {
+                  position: "top-center",
+                }),
+            }
+          );
+        },
+        onError: () => {
+          toast.error("Error Booking", {
+            position: "top-center",
+          });
+        },
       }
     );
   }
 
   function handleCashPay() {
-    if (data?.role === "customer" && token) {
-      navigate("/CustomerDashboard");
-    } else {
-      navigate("/Login?role=customer");
-    }
+    setIsCreatingBooking(true);
+    createBookings(
+      {
+        endpoint: "/api/create-booking",
+        payload: { ...bookingData, paymentMethod: "cash" },
+      },
+      {
+        onSuccess: (data) => {
+          toast.success("Booking Created Successfully", {
+            position: "top-center",
+          });
+          setIsCreatingBooking(false);
+          if (data?.role === "customer" && token) {
+            navigate("/CustomerDashboard");
+          } else {
+            navigate("/Login?role=customer");
+          }
+        },
+        onError: () => {
+          toast.error("Error Booking", {
+            position: "top-center",
+          });
+          setIsCreatingBooking(false);
+        },
+      }
+    );
   }
+
   return (
     <section className=" min-h-[100dvh] flex  flex-col items-center  p-4 ">
       <Title
@@ -65,15 +113,15 @@ const CheckoutPage = () => {
       <div className="max-w-4xl w-full rounded-xl mx-auto p-6 bg-[#FFFFFF]">
         <div className="mb-4">
           <label className="block text-lg font-medium">Booking Name:</label>
-          <p className="text-sm">{name}</p>
+          <p className="text-sm">{name || data?.name}</p>
         </div>
         <div className="mb-4">
           <label className="block text-lg font-medium">Email:</label>
-          <p className="text-sm">{email}</p>
+          <p className="text-sm">{email || data?.email}</p>
         </div>
         <div className="mb-4">
           <label className="block text-lg font-medium">Phone:</label>
-          <p className="text-sm">{phone}</p>
+          <p className="text-sm">{phone || data?.phone}</p>
         </div>
         <div className="mb-4">
           <label className="block text-lg font-medium">Booking Date:</label>
@@ -100,7 +148,7 @@ const CheckoutPage = () => {
       </div>
       <div className="flex w-full gap-4 max-w-4xl">
         <Button
-          loading={isPending}
+          loading={isCreatingBookins || isPending}
           loaderProps={{ type: "bars" }}
           disabled={isPending}
           onClick={handlePayment}
@@ -110,6 +158,9 @@ const CheckoutPage = () => {
         </Button>
         {paymentMethod === "cash" && (
           <Button
+            loading={isCreatingBooking}
+            loaderProps={{ type: "bars" }}
+            disabled={isPending}
             onClick={handleCashPay}
             className="!bg-black !text-white mt-4 !w-full !max-w-4xl   !h-[41px] !px-[40px] !py-[10px] !rounded-lg  hover:!bg-black/80 !transition-all !duration-300 !cursor-pointer"
           >
