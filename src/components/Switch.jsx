ef1a34341209747d6cc50b9f2f3e4cdc031a6c58
Mubashir <m.mubashirweb@gmail.com>
2025-06-03 10:50:27 +0500

    update half

--- src/components/Switch.jsx
+++ src/components/Switch.jsx
@@ -1,6 +1,38 @@
 import { Switch } from "@mantine/core";
+import { useQueryHook, useUpdateMutation } from "../services/reactQuery";
+import { toast } from "react-toastify";
 
 export function SwitchCom() {
+  const { id } = JSON.parse(localStorage.getItem("data"));
+  // /users
+  const { data: notify, isLoading: isFetechingNotification } = useQueryHook({
+    queryKey: "email-notification",
+    endpoint: `/api/users/${id}/email-preferences`,
+    staleTime: 0 * 60 * 1000,
+  });
+
+  console.log(notify, "....notify");
+  //
+  const { mutate: updateLocation } = useUpdateMutation("email-notification");
+  function handleUpdateNotification() {
+    updateLocation(
+      {
+        endpoint: `/api/users/${{}}/email-preferences`,
+        payload: [],
+      },
+      {
+        onSuccess: () =>
+          toast.success("Notifications Setting Successfully", {
+            position: "top-center",
+          }),
+        onError: () =>
+          toast.error("Error Updated Setting", {
+            position: "top-center",
+          }),
+      }
+    );
+  }
+
   return (
     <Switch
       defaultChecked
